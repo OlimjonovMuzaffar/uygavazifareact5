2ed468b010e8029afdb267402f40692b27eb1042
OlimjonovMuzaffar <kanaluchun06@gmail.com>
2023-10-01 19:01:28 +0500

    weewwe

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ function App() {
   const [trips, setData] = useState(null);
 
   useEffect(() => {
-    fetch("http://localhost:3000/trips")
+    fetch("https://dummyjson.com/products")
       .then((e) => {
         return e.json();
       })
       .then((e) => {
-        setData(e);
+        const mahsulotlar = e.products.slice(0, 4);
+        setData(mahsulotlar);
+        console.log(mahsulotlar);
       })
       .catch((e) => {
         console.log(e);
@@ -19,17 +21,28 @@ function App() {
 
   return (
     <div className="mt-15 ">
-      <h1 className="text-4xl font-semibold font-mono" >Travel</h1>
-      <ul className="flex gap-8 	flex-col justify-center items-center  p-8   ">
+      <h1 className="text-4xl font-semibold font-mono">Phones</h1>
+      <ul className="flex gap-8 	flex-col justify-center items-center  p-8 cursor: pointer  ">
         {trips &&
           trips.map((trip) => {
             return (
-              <li key={trip.title} className="shadow-lg shadow-indigo-500/40 rounded-md p-4 ">
-                <h2 className="  text-xl font-semibold font-mono">
+              <li
+                key={trip.title}
+                className=" w-80 shadow-lg shadow-indigo-500/40 rounded-md p-4 cursor-pointer"
+              >
+                <h1 className=" text-xl font-semibold font-serif">
                   {trip.title}
+                </h1>
+                <h2 className="  text-xl font-semibold font-mono">
+                  {trip.description}
                 </h2>
-                <h3 className="  text-xl font-normal italic">{trip.price}</h3>
-                <h4 className="text-xl font-medium font-serif">{trip.loc}</h4>
+                <img src={trip.thumbnail} alt="" />
+                <h3 className="  text-xl font-normal font-medium">
+                  Price: {trip.price}
+                </h3>
+                <h4 className="text-xl font-medium font-serif">
+                  Rating: {trip.rating}
+                </h4>
               </li>
             );
           })}
